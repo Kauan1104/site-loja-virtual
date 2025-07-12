cd0b8929f48c57d53f15ed55463004c6cf4c8a7a
kauan faller <kauanFa1104@gmail.com>
2025-07-12 17:48:37 -0300

    Adiciona suporte para Render e salvar dados com backend

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 
 app.use(express.json({ limit: '10mb' }));
 app.use(express.static(path.join(__dirname, 'public')));
